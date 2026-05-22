e66bae099919e5601439d1480a7a9236980cb2ec
Sonali Barik <sonalibarik190@gmail.com>
2026-05-22 14:39:36 +0530

    Add files via upload

---
+++ SecondLastElement.java
@@ -0,0 +1,8 @@
+// 10. Java program to print the 2nd last element of an array
+public class SecondLastElement {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+
+        System.out.println("Output: " + arr[arr.length - 2]);
+    }
+}
