d5ac95131846a35533c57ee7faaef991c8e9efeb
Sonali Barik <sonalibarik190@gmail.com>
2026-05-25 14:48:44 +0530

    Add files via upload

---
+++ ArrayCopys.java
@@ -0,0 +1,16 @@
+//45
+public class ArrayCopys {
+    public static void main(String[] args) {
+
+        int a[] = {1,2,3,4,5};
+        int b[] = new int[a.length];
+
+        System.arraycopy(a, 0, b, 0, a.length);
+
+        System.out.print("Copied Array: ");
+
+        for(int i=0; i<b.length; i++) {
+            System.out.print(b[i] + " ");
+        }
+    }
+}
