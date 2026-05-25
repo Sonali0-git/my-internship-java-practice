83e9f9fe463bbbc78f07b4a8e6bfbd1783d1e673
Sonali Barik <sonalibarik190@gmail.com>
2026-05-25 17:13:34 +0530

    Add files via upload

---
+++ Prime1ToN.java
@@ -0,0 +1,21 @@
+//17. Prime Numbers from 1 to N
+class Prime1ToN {
+    public static void main(String[] args) {
+
+        int n = 20;
+
+        for(int i=2; i<=n; i++) {
+
+            int count = 0;
+
+            for(int j=1; j<=i; j++) {
+
+                if(i % j == 0)
+                    count++;
+            }
+
+            if(count == 2)
+                System.out.print(i + " ");
+        }
+    }
+}
