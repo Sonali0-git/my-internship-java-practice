ce0c1837066924f1dbc09ff30d57b64be217902b
Sonali Barik <sonalibarik190@gmail.com>
2026-05-25 15:11:46 +0530

    Add files via upload

---
+++ AscendingOrder.java
@@ -0,0 +1,24 @@
+//48. AscendingOrder
+public class AscendingOrder{
+    public static void main(String[] args) {
+
+        int a[] = {9,5,6,1,2,7};
+
+        for(int i=0; i<a.length; i++) {
+            for(int j=i+1; j<a.length; j++) {
+
+                if(a[i] > a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+
+        System.out.print("Ascending Order: ");
+
+        for(int i=0; i<a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
