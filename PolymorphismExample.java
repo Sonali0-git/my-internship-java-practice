dcdc7a3d9e77645580777dab97ca68b086d4773b
Sonali Barik <sonalibarik190@gmail.com>
2026-05-26 15:05:33 +0530

    Add files via upload

---
+++ PolymorphismExample.java
@@ -0,0 +1,31 @@
+class Shape {
+
+    void draw() {
+        System.out.println("Drawing shape");
+    }
+}
+
+class Circle extends Shape {
+
+    void draw() {
+        System.out.println("Drawing circle");
+    }
+}
+
+class Rectangle extends Shape {
+
+    void draw() {
+        System.out.println("Drawing rectangle");
+    }
+}
+
+public class PolymorphismExample {
+    public static void main(String[] args) {
+
+        Shape s1 = new Circle();
+        Shape s2 = new Rectangle();
+
+        s1.draw();
+        s2.draw();
+    }
+}
